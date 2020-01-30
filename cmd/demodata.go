f5e46be7dbfff8d28c370ef420e4ba83336d3b9f
Jörg Kütemeier <joerg@kuetemeier.de>
2020-01-30 18:06:11 +0100

    Simplify demodata.

--- cmd/demodata.go
+++ cmd/demodata.go
@@ -54,34 +54,34 @@ var demodataCmd = &cobra.Command{
 			}
 
 			outputFeed.Items = []*feeds.Item{
-				&feeds.Item{
+				{
 					Title:       "Demo 1 - Feed Item One",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-one/"},
 					Description: "A wonderful demo feed item.",
 					Author:      &feeds.Author{Name: "John Müller", Email: "jmueller@demo.de"},
 					Created:     now,
 				},
-				&feeds.Item{
+				{
 					Title:       "Demo 1 - Feed Item Two",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-two/"},
 					Description: "More thoughts on demo feed items.",
 					Created:     now.AddDate(0, 0, -1),
 					Updated:     now,
 				},
-				&feeds.Item{
+				{
 					Title:       "Demo 1 - Feed Item Three",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-three/"},
 					Description: "And another <em>great</em> demo feed item.",
 					Created:     now.AddDate(0, 0, -2),
 				},
-				&feeds.Item{
+				{
 					Title:       "Double Item 1 in Demo 1 and 2",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-double-1/"},
 					Description: "This item has the same title and link in Demo 1 and 2.",
 					Created:     now.AddDate(0, 0, -3),
 					Updated:     now.AddDate(0, 0, -2),
 				},
-				&feeds.Item{
+				{
 					Title:       "Double Item 2 in Demo 1 and 2 - First version",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-double-2/"},
 					Description: "This item has the same link, but different title in Demo 1 and 2.",
@@ -99,33 +99,33 @@ var demodataCmd = &cobra.Command{
 			}
 
 			outputFeed.Items = []*feeds.Item{
-				&feeds.Item{
+				{
 					Title:       "Demo 2 - Feed Item One",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-a/"},
 					Description: "Call this a wonderful demo feed item.",
 					Author:      &feeds.Author{Name: "John Müller", Email: "jmueller@demo.de"},
 					Created:     now,
 				},
-				&feeds.Item{
+				{
 					Title:       "Demo 2 - Feed Item Two",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-b/"},
 					Description: "And even more thoughts on demo feed items.",
 					Created:     now.AddDate(0, 0, -1),
 				},
-				&feeds.Item{
+				{
 					Title:       "Demo 2 - Feed Item Three",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-c/"},
 					Description: "Can you believe it? Another <em>great</em> demo feed item.",
 					Created:     now.AddDate(0, 0, -2),
 				},
-				&feeds.Item{
+				{
 					Title:       "Double Item 1 in Demo 1 and 2",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-double-1/"},
 					Description: "This item has the same title and link in Demo 1 and 2.",
 					Created:     now.AddDate(0, 0, -3),
 					Updated:     now.AddDate(0, 0, -2),
 				},
-				&feeds.Item{
+				{
 					Title:       "Double Item 2 in Demo 1 and 2 - Second version",
 					Link:        &feeds.Link{Href: "https://demo/blog/item-double-2/"},
 					Description: "This item has the same link, but different title in Demo 1 and 2.",
